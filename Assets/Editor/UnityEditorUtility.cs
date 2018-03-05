f69b6358b277f4c57478deeafa285b9841a1a98a
guangpingmo@qq.com <guangpingmo@qq.com>
2018-03-06 00:20:23 +0800

    Add CleanPlayerPrefs and CleanEditorPrefs utility to menu.

---
+++ Assets/Editor/UnityEditorUtility.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public static class UnityEditorUtility{
+    #region menu extension.
+    [MenuItem("Utility/Prefs/CleanPlayerPrefs")]
+    public static void CleanPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    [MenuItem("Utility/Prefs/CleanEditorPrefs")]
+    public static void CleanEditorPrefs()
+    {
+        EditorPrefs.DeleteAll();
+    }
+    #endregion
+}
